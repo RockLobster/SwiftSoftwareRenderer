9543aee99fe7c06da4748f3658b6f2afa3d50454
Rainer Schlönvoigt <rainer.schlonvoigt@philips.com>
2015-11-01 14:08:17 +0100

    initial commit with quick and nimble testing frameworks

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :osx, '10.11’
+
+use_frameworks!
+
+def testing_pods
+    pod 'Quick', '~> 0.8.0'
+    pod 'Nimble', '3.0.0'
+end
+
+target 'SwiftSoftwareRendererTests' do
+	testing_pods
+end
---
+++ SwiftSoftwareRenderer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftSoftwareRenderer
+//
+//  Created by Rainer Schlönvoigt on 01/11/15.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
